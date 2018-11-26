33741a68ffffbdedbb776feb195dcba0f085aef2
drood87 <dbernstein1987@hotmail.com>
2018-11-26 17:22:18 +1100

    added first consts and function

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-const team1scores = document.querySelectorAll("#team1score").values;
+const team1scores = Array.from(document.querySelectorAll("#team1score"));
+const team2scores = document.querySelectorAll("#team2scores");
+const team3scores = document.querySelectorAll("#team3scores");
+
+const button = document.querySelector("button");
+
+function calculateWinner() {
+  const totalScoreTeam1 = team1scores
+    .map(values => values.value)
+    .reduce((total, scores) => {
+      return total + scores;
+    }, 0);
+  console.log(totalScoreTeam1);
+}
+
+button.addEventListener("click", calculateWinner);
