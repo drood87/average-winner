db15e8de435c34cb79d39dfc6d483fd86b8c397f
drood <dbernstein1987@hotmail.com>
2018-11-26 19:33:19 +1100

    added script to calculate average of the score of three teams

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
 const team1scores = Array.from(document.querySelectorAll("#team1score"));
-const team2scores = document.querySelectorAll("#team2scores");
-const team3scores = document.querySelectorAll("#team3scores");
+const team2scores = Array.from(document.querySelectorAll("#team2score"));
+const team3scores = Array.from(document.querySelectorAll("#team3score"));
 
 const button = document.querySelector("button");
 
 function calculateWinner() {
-  const totalScoreTeam1 = team1scores
-    .map(values => values.value)
-    .reduce((total, scores) => {
-      return total + scores;
-    }, 0);
-  console.log(totalScoreTeam1);
+    const totalScoreTeam1 = team1scores
+        .map(values => Number(values.value))
+        .reduce((total, scores) => {
+            return total + scores;
+        }, 0);
+
+    const totalScoreTeam2 = team2scores
+        .map(values => Number(values.value))
+        .reduce((total, scores) => {
+            return total + scores;
+        }, 0);
+
+    const totalScoreTeam3 = team3scores
+        .map(values => Number(values.value))
+        .reduce((total, scores) => {
+            return total + scores;
+        }, 0);
+
+    const averageTeam1 = totalScoreTeam1 / 3;
+    const averageTeam2 = totalScoreTeam2 / 3;
+    const averageTeam3 = totalScoreTeam3 / 3;
 }
 
 button.addEventListener("click", calculateWinner);
